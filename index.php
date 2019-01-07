dd7358d24d2b501f28d22a70fd7a47b2c6a9e4de
bishwajit1965 <paul.bishwajit09@gmail.com>
2019-01-07 16:36:16 +0600

    All the files added

---
+++ index.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html class="no-js" lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<title>Portfolio site</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="manifest" href="site.webmanifest">
+		<link rel="apple-touch-icon" href="icon.png">
+		<!-- Place favicon.ico in the root directory -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/fontawesome.all.css">
+		<link rel="stylesheet" type="text/css" href="css/fontawesome.all.css">
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<!-- Site code below -->
+		<header id="header" class="">
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-sm-4 col-md-4 col-lg-4">
+						<img src="img/bishwajit1.jpg" alt="Author's image">
+					</div>
+					<div class="col-sm-8 col-md-8 col-lg-8">
+						<div class="d-flex flex-column">
+							<div class="p-4 bg-dark text-white">
+								<div class="d-flex flex-row justify-content-between align-items-center">
+									<h4 class="display-5">Bishwajit Paul</h4>
+									<a href="" class="text-white"><i class="fab fa-2x fa-facebook-square"></i></a>
+									<a href="" class="text-white"><i class="fab fa-2x fa-linkedin"></i></a>
+									<a href="" class="text-white"><i class="fab fa-2x fa-google-plus"></i></a>
+									<a href="" class="text-white"><i class="fab fa-2x fa-twitter"></i></a>
+								</div>
+							</div>
+							<div class="p-3 text-white bg-black">
+								Web Development Portfolio
+							</div>
+							<div class="d-flex flex-row text-center text-white">
+								<div class="col-sm-3 col-md-3 col-lg-3 p-4 bg-success font-div" data-toggle="collapse" data-target="#home">
+									<i class="fas fa-home fa-2x d-block"></i> Home
+								</div>
+								<div class="col-sm-3 col-md-3 col-lg-3 p-4 bg-danger font-div" data-toggle="collapse" data-target="#resume">
+									<i class="fas fa-graduation-cap fa-2x d-block"></i> Resume
+								</div>
+								<div class="col-sm-3 col-md-3 col-lg-3 p-4 bg-primary font-div" data-toggle="collapse" data-target="#works">
+									<i class="fas fa-folder fa-2x d-block"></i> Works
+								</div>
+								<div class="col-sm-3 col-md-3 col-lg-3 p-4 bg-warning font-div" data-toggle="collapse" data-target="#contact">
+									<i class="fas fa-envelope fa-2x d-block"></i> Contact
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</header>
+			<div class="container">
+				<div class="collapse" id="home">
+					<div class="p-5 text-white bg-success">
+						<h1>Resume</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+					</div>
+					<div class="card card-body">
+						<h2>Skills</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+						<h4>HTML</h4>
+						<div class="progress mb-4">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:100%;"></div>
+						</div>
+						<h4>CSS</h4>
+						<div class="progress mb-4">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:90%;"></div>
+						</div>
+						<h4>PHP</h4>
+						<div class="progress mb-4">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:80%;"></div>
+						</div>
+						<h4>BOOTSTRAP</h4>
+						<div class="progress mb-4">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:75%;"></div>
+						</div><h4>JQUERY</h4>
+						<div class="progress mb-4">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" style="width:70%;"></div>
+						</div>
+					</div>
+				</div>
+				<div class="collapse" id="resume">
+					<div class="p-5 text-white bg-danger">
+						<h1>My Resume</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+					</div>
+					<div class="card card-body">
+						<h2>Where I have worked ? </h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+						<div class="card-deck text-center">
+							<div class="card">
+								<div class="card-header text-uppercase font-weight-bold">WEB DEVELOPER</div>
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<p class="bg-dark text-white p-2"> Worked as a front-end developer</p>
+									<p class="bg-dark text-white p-2"> Worked as a front-end developer</p>
+								</div>
+								<div class="card-footer">Date: 2012-2018</div>
+							</div>
+							<div class="card">
+								<div class="card-header text-uppercase font-weight-bold">WEB DEVELOPER</div>
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<p class="bg-dark text-white p-2"> Worked as a front-end developer</p>
+									<p class="bg-dark text-white p-2"> Worked as a front-end developer</p>
+								</div>
+								<div class="card-footer">Date: 2012-2018</div>
+							</div>
+							<div class="card">
+								<div class="card-header text-uppercase font-weight-bold">WEB DEVELOPER</div>
+								<div class="card-body">
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+									<p class="bg-dark text-white p-2"> Worked as a front-end developer</p>
+									<p class="bg-dark text-white p-2"> Worked as a front-end developer</p>
+								</div>
+								<div class="card-footer">Date: 2012-2018</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="collapse" id="works">
+					<div class="p-5 text-white bg-primary">
+						<h1>My Portfolio</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+					</div>
+					<div class="card card-body">
+						<h2>What have I built ?</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+						<div class="row no-gutters">
+							<div class="col-sm-3 col-md-3 col-lg-3 order-4"><img src="img/img1.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3"><img src="img/img2.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3"><img src="img/img3.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3"><img src="img/img3.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3"><img src="img/img5.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3 order-1" ><img src="img/img6.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3"><img src="img/img7.png" alt="Image One"></div>
+							<div class="col-sm-3 col-md-3 col-lg-3 order-8"><img src="img/img8.png" alt="Image One"></div>
+						</div>
+					</div>
+				</div>
+				<div class="collapse" id="contact">
+					<div class="p-5 text-white bg-warning">
+						<h1>Contact me</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+					</div>
+					<div class="card card-body">
+						<h2>What have I built ?</h2>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, ipsam deleniti inventore veniam recusandae quo minus, asperiores saepe voluptatibus, enim odio consequuntur praesentium adipisci ea, illum reprehenderit rem eveniet dignissimos.</p>
+						<div class="card card-body">
+							<div class="row">
+								<div class="col-sm-8 col-md-8 col-lg-8 offset-2">
+									<h3>Contact us</h3>
+									<form action="" method="post" accept-charset="utf-8">
+										<div class="form-group">
+											<div class="input-group input-group-md">
+												<span class="input-group-addon bg-warning text-white p-1">
+													<i class="fas fa-user"></i></span>
+													<input type="text" name="" class="form-control bg-dark text-white" placeholder="Input name....">
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="input-group input-group-md">
+												<span class="input-group-addon bg-warning text-white p-1">
+													<i class="fas fa-envelope"></i></span>
+													<input type="email" name="email" class="form-control bg-dark text-white" placeholder="Input email...">
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="input-group input-group-md">
+												<span class="input-group-addon bg-warning text-white p-1">
+													<i class="fas fa-pen"></i></span>
+													<input type="email" name="email" class="form-control bg-dark text-white" placeholder="Input message...">
+											</div>
+										</div>
+										<div class="form-group">
+											<div class="input-group input-group-md">
+												<span class="input-group-addon bg-warning text-white p-1">
+													<i class="fas fa-upload"></i></span>
+												<button type="submit" class="btn btn-md btn-primary"> Submit</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="footer p-5 bg-dark text-center">
+					<button class="btn"> <i class="fa fa-download fa-2x"></i> Download</button>
+				</div>
+			</div>
+				<!-- Javascript -->
+				<script src="js/vendor/jquery-3.3.1.min.js"></script>
+				<script src="js/popper.min.js"></script>
+				<script src="js/vendor/modernizr-3.6.0.min.js"></script>
+				<script src="js/bootstrap.min.js"></script>
+				<script src="js/plugins.js"></script>
+				<script src="js/main.js"></script>
+				<!-- Loader of specific sections and releasing other opened sections-->
+				<script type="text/javascript">
+					$('.font-div').click(function(){
+						$('.collapse').collapse('hide');
+					});
+				</script>
+			</body>
+		</html>
